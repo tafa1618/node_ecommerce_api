7c85e69daf7747b4132823dd6f223c03f7f861c7
tafa1618 <64489795+tafa1618@users.noreply.github.com>
2021-12-21 21:07:39 +0000

    connecting with the db

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import electromenager from './data/electromenager.js';
+import connectDB from './config/db.js';
 
+const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Welcome to our API');
@@ -13,7 +16,9 @@ app.get('/api/electromenager', (req, res) => {
   res.json(electromenager);
 });
 app.get('/api/electromenager/:id', (req, res) => {
-  const product = electromenager.find((p) => p.id === req.params.id);
+  const product = electromenager.find((p) => {
+    p.id === req.params.id;
+  });
   res.json(product);
 });
 app.listen(port, console.log(`app listening on port ${port}`));
